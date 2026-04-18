427c46946cb81f0b6b2cbaa34016e4ef2404e065
Reyansh <reyansh.sh07@gmail.com>
2026-04-18 06:32:59 +0530

    11th file practise

--- 11-form-handling/src/App.jsx
+++ 11-form-handling/src/App.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 const App = () => {
 
-  const submitHandler = (e) => {
+  function submitHandler(e){
     e.preventDefault()
     console.log('Form Submitted');
   }
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        submitHandler(e)
-      }}>
+      <form onSubmit={submitHandler}>
         <input type="text" placeholder='Enter your name' />
         <button>Submit</button>
       </form>
